da613a8970071238509972090cbc813e14fb9e1b
rishika <rishikajain290@gmail.com>
2023-03-28 05:12:39 +0530

    final commit

---
+++ app/src/main/java/com/example/myapplication/Adapters/UpdateVertical.java
@@ -0,0 +1,9 @@
+package com.example.myapplication.Adapters;
+
+import com.example.myapplication.models.HomeVerModel;
+
+import java.util.ArrayList;
+
+public interface UpdateVertical {
+    public void callback(int position, ArrayList<HomeVerModel> list);
+}
